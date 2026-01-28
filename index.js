e7f69a94b7a5dfefb30aedfc05a29b9fa434b09f
Florian Gerard <Florian.Gerard@economie.fgov.be>
2026-01-28 10:30:05 +0100

    add port

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const server = createServer(async (req, res) => {
     }
 });
 
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-    console.log('Listening on 127.0.0.1:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Listening on port ${PORT}`);
 });
